3928c7eda93ed02befb8cfd922ffa851a3ceb12c
Stephen Sagarino Jr <113438801+ReidoBoss@users.noreply.github.com>
2024-05-14 09:48:49 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,18 @@
-const {createConnection } = require('mysql');
-const con = createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"mrs"
+const { createPool } = require("mysql");
+
+// const con = createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "megaland",
+// });
+
+const con = createPool({
+  host: "srv545.hstgr.io",
+  user: "u484898470_megaland",
+  password: "Concentrix70139",
+  database: "u484898470_megaland_db",
 });
 
-con.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-  });
-
+// con.connect((error) => {s
 module.exports = con;
